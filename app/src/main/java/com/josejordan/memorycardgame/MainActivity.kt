52f4d3d4dbde68b5c52c920eb133ed585826f0a2
Jose Jordan <mundodigital.pro@gmail.com>
2023-04-20 14:09:46 +0200

    Attempts updated on new game

--- app/src/main/java/com/josejordan/memorycardgame/MainActivity.kt
+++ app/src/main/java/com/josejordan/memorycardgame/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.josejordan.memorycardgame
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.View
 import android.widget.Button
 import android.widget.GridLayout
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,38 +30,109 @@ class MainActivity : AppCompatActivity() {
         attemptCounter = findViewById(R.id.attempt_counter)
         gridLayout = findViewById(R.id.gridLayout)
 
-        setupCards()
+        val newGameButton = findViewById<Button>(R.id.new_game_button)
+        newGameButton.setOnClickListener {
+            resetGame()
+            showSettingsDialog()
+        }
+
+        val resetButton = findViewById<Button>(R.id.reset_button)
+        resetButton.setOnClickListener {
+            resetGame()
+        }
+
+
         shuffleCards()
         setupButtonClickListeners()
+        showSettingsDialog()
     }
 
-        private fun setupButtonClickListeners() {
+    private fun setupButtonClickListeners() {
         for (i in 0 until gridLayout.childCount) {
             val button = gridLayout.getChildAt(i) as Button
             button.setOnClickListener { onCardClick(button) }
             buttons.add(button)
         }
     }
-
-    private fun setupCards() {
+    private fun setupCards(cardCount: Int) {
+        gridLayout.removeAllViews()
         val cardImages = arrayOf(
             R.drawable.card1,
             R.drawable.card2,
+            R.drawable.card3,
+            R.drawable.card4,
+            R.drawable.card5,
+            R.drawable.card6,
+            R.drawable.card7,
+            R.drawable.card8,
+
         )
-        for (card in cardImages) {
-            cardPairs.add(Pair(card, card))
-            cardPairs.add(Pair(card, card))
+
+        cardPairs.clear()
+
+        if (cardCount <= cardImages.size * 2) {
+            for (i in 0 until cardCount / 2) {
+                cardPairs.add(Pair(cardImages[i % cardImages.size], cardImages[i % cardImages.size]))
+                cardPairs.add(Pair(cardImages[i % cardImages.size], cardImages[i % cardImages.size]))
+            }
+            cardPairs.shuffle()
+        } else {
+            Toast.makeText(this, "Número de cartas inválido", Toast.LENGTH_SHORT).show()
         }
-        cardPairs.shuffle()
     }
 
     private fun shuffleCards() {
+        // Elimina todos los botones del GridLayout antes de agregar nuevos
+        gridLayout.removeAllViews()
+
         cardPairs.shuffle()
-        for (i in 0 until gridLayout.childCount) {
-            val button = gridLayout.getChildAt(i) as Button
-            button.tag = cardPairs[i].first
-                       button.setBackgroundResource(R.drawable.card_back)
+        for (i in 0 until cardPairs.size) {
+            val button = Button(this).apply {
+                layoutParams = GridLayout.LayoutParams().apply {
+                    width = resources.getDimensionPixelSize(R.dimen.card_width)
+                    height = resources.getDimensionPixelSize(R.dimen.card_height)
+                    setMargins(5, 5, 5, 5)
+                }
+                tag = cardPairs[i].first
+                setBackgroundResource(R.drawable.card_back)
+                setOnClickListener { onCardClick(this) }
+            }
+            gridLayout.addView(button)
         }
+    }
+
+    private fun showSettingsDialog() {
+        val builder = AlertDialog.Builder(this)
+        val view = layoutInflater.inflate(R.layout.settings_dialog, null)
+        builder.setView(view)
+
+        val radioGroup = view.findViewById<RadioGroup>(R.id.radio_group)
+        val applyButton = view.findViewById<Button>(R.id.apply_button)
+
+        val dialog = builder.create()
+        applyButton.setOnClickListener {
+            when (radioGroup.checkedRadioButtonId) {
+                R.id.radio_4 -> {
+                    setupCards(4)
+                    gridLayout.columnCount = 2
+                    gridLayout.rowCount = 2
+                }
+                R.id.radio_6 -> {
+                    setupCards(6)
+                    gridLayout.columnCount = 3
+                    gridLayout.rowCount = 2
+                }
+                R.id.radio_16 -> {
+                    setupCards(16)
+                    gridLayout.columnCount = 4
+                    gridLayout.rowCount = 4
+                }
+            }
+            shuffleCards()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun onCardClick(button: Button) {
@@ -71,7 +142,7 @@ class MainActivity : AppCompatActivity() {
             firstCard = button
         } else {
             attempts++
-            attemptCounter.text = "Intentos: $attempts"
+            attemptCounter.text = getString(R.string.intentos_label, attempts)
             secondCard = button
             isProcessing = true
             checkMatch()
@@ -107,7 +178,7 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "¡Has ganado!", Toast.LENGTH_LONG).show()
     }
 
-    fun resetGame(view: View) {
+    private fun resetGame() {
         shuffleCards()
         for (button in buttons) {
             button.setBackgroundResource(R.drawable.card_back)
